8b67e640cc69159f4ad6a569424deeacef6c99ff
EE <emmerich@126.com>
2015-12-15 10:12:38 +0800

    想做个单点登录的demo。 应用本身提供单点登录的接口。--未完成， 先合并一下昨晚在家里笔记本push的代码。

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# DOA
-算系一个OA项目， 然后通过activiti做OA。
+<h1>DOA</h1>
 
+<p>算系一个OA项目， 然后通过activiti做OA。</p>
+
+<p>
 基于springMVC。
 从https://github.com/emmerichLuang/singleBbs 这个项目拷贝过来的骨架。毕竟bbs还搞个毛线的工作流啊。
-
+</p>
---
+++ src/main/java/dOA/controller/api/SSOLoginController.java
@@ -0,0 +1,22 @@
+package dOA.controller.api;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import dOA.framework.controller.BaseController;
+
+
+@Controller
+public class SSOLoginController extends BaseController {
+
+	@ResponseBody
+	@RequestMapping(value = { "/sso/login.json" }, method = { RequestMethod.POST })	
+	public void login(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+}
